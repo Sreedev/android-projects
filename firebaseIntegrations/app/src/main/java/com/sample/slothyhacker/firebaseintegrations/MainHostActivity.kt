69dba3c867a02c2d8577f1712736e1a51d4dcb5d
Sreedev <sreedev.r@niihilent.com>
2019-11-08 08:34:12 +0530

    - Changed structure - Added Navigation graph - Added list vie

--- firebaseIntegrations/app/src/main/java/com/sample/slothyhacker/firebaseintegrations/MainHostActivity.kt
+++ firebaseIntegrations/app/src/main/java/com/sample/slothyhacker/firebaseintegrations/MainHostActivity.kt
@@ -1,12 +1,12 @@
 package com.sample.slothyhacker.firebaseintegrations
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainHostActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_integration_list)
     }
 }
