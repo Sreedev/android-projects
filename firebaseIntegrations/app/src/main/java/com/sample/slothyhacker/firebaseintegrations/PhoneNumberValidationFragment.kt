1f021ec9a7ebcdc36b0bf2126af3f1e636e67ce9
Sreedev <sreedev.r5@gmail.com>
2019-11-12 21:49:17 +0530

    Firebase basic implementation. Not working yet.

--- firebaseIntegrations/app/src/main/java/com/sample/slothyhacker/firebaseintegrations/PhoneNumberValidationFragment.kt
+++ firebaseIntegrations/app/src/main/java/com/sample/slothyhacker/firebaseintegrations/PhoneNumberValidationFragment.kt
@@ -1,17 +1,87 @@
 package com.sample.slothyhacker.firebaseintegrations
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.*
+import kotlinx.android.synthetic.main.fragment_email_auth.*
+import java.util.concurrent.TimeUnit
 
 class PhoneNumberValidationFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    private lateinit var mAuth:  FirebaseAuth
+    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+    private var verificationInProgress = false
+    private var storedVerificationId: String? = ""
+    private lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mAuth = FirebaseAuth.getInstance()
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                verificationInProgress = false
+                showSnackbar("STATE_VERIFY_SUCCESS")
+                signInWithPhoneAuthCredential(credential)
+            }
+            override fun onVerificationFailed(e: FirebaseException) {
+                verificationInProgress = false
+                if (e is FirebaseAuthInvalidCredentialsException) {
+                    showSnackbar("Invalid phone number.")
+                } else if (e is FirebaseTooManyRequestsException) {
+                   showSnackbar("Quota exceeded.")
+                }
+                showSnackbar("STATE_VERIFY_FAILED")
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken) {
+                Log.d(TAG, "onCodeSent:$verificationId")
+                storedVerificationId = verificationId
+                resendToken = token
+                showSnackbar("State code sent")
+            }
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_phone_number_validation, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startPhoneNumberVerification("+91 9643897450")
+    }
+
+    private fun startPhoneNumberVerification(phoneNumber: String) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(phoneNumber, 60, TimeUnit.SECONDS, activity!!, callbacks)
+        verificationInProgress = true
+    }
+
+    private fun showSnackbar(message: String) {
+        Snackbar.make(main_container, message, Snackbar.LENGTH_LONG).show()
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        mAuth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                showSnackbar("signInWithCredential:success")
+            } else {
+                // Sign in failed, display a message and update the UI
+                Log.w(TAG, "signInWithCredential:failure", it.exception)
+                if (it.exception is FirebaseAuthInvalidCredentialsException) {
+                    showSnackbar("Invalid code.")
+                }
+                showSnackbar("Sign in failed")
+            }
+        }
     }
 }
