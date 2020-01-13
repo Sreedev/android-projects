4b63857e1f457e19ea3697fe3c3c3932b633e8fd
Sreedev <sreedev.r5@gmail.com>
2020-01-13 11:50:14 +0530

    Implemented on device and cloud object detection

--- firebaseIntegrations/app/src/main/java/com/sample/slothyhacker/firebaseintegrations/ObjectTrackingFragment.kt
+++ firebaseIntegrations/app/src/main/java/com/sample/slothyhacker/firebaseintegrations/ObjectTrackingFragment.kt
@@ -6,11 +6,16 @@ import android.content.res.Resources
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_face_detection.*
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.objects.FirebaseVisionObject
+import com.google.firebase.ml.vision.objects.FirebaseVisionObjectDetectorOptions
 import kotlinx.android.synthetic.main.fragment_face_detection.takePicture
 import kotlinx.android.synthetic.main.fragment_object_tracking.*
 
@@ -35,10 +40,18 @@ class ObjectTrackingFragment : Fragment() {
         }
 
         find_objects.setOnClickListener {
-            view?.let { runObjectDetection()}
+            view?.let {
+                runOnDeviceObjectDetection()
+            }
         }
-        find_objects.isEnabled = false
+
+        find_objects_name.setOnClickListener {
+            view?.let {
+                runOnCloudObjectDetection()
+            }
+        }
     }
+
     private fun takePictureForObjects() {
         // Using Camera app take a picture
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
@@ -61,11 +74,73 @@ class ObjectTrackingFragment : Fragment() {
 
             // Set image and button enabled for face detection
             image_view.setImageBitmap(cameraImage)
-            find_objects.isEnabled = true
         }
     }
 
-    private fun runObjectDetection() {
+    private fun runOnDeviceObjectDetection() {
+        val image = FirebaseVisionImage.fromBitmap(cameraImage)
 
+        val options = FirebaseVisionObjectDetectorOptions.Builder()
+            .setDetectorMode(FirebaseVisionObjectDetectorOptions.SINGLE_IMAGE_MODE)
+            .enableMultipleObjects()
+            .enableClassification()
+            .build()
+        val detector = FirebaseVision.getInstance().getOnDeviceObjectDetector(options)
+
+        detector.processImage(image)
+            .addOnSuccessListener { objects ->
+                // Task completed successfully
+                val LOG_MOD = "MLKit-ODT"
+                for ((idx, obj) in objects.withIndex()) {
+                    val box = obj.boundingBox
+                    Log.d(LOG_MOD, "Detected object: ${idx} ")
+                    when (obj.classificationCategory) {
+                        0 -> Log.d(LOG_MOD, "  Classification name: CATEGORY_UNKNOWN")
+                        1 -> Log.d(LOG_MOD, "  Classification name: CATEGORY_HOME_GOOD")
+                        2 -> Log.d(LOG_MOD, "  Classification name: CATEGORY_FASHION_GOOD")
+                        3 -> Log.d(LOG_MOD, "  Classification name: CATEGORY_FOOD")
+                        4 -> Log.d(LOG_MOD, "  Classification name: CATEGORY_PLACE")
+                        5 -> Log.d(LOG_MOD, "  Classification name: CATEGORY_PLANT")
+                    }
+                    Log.d(LOG_MOD, "  Classification code: ${obj.classificationCategory}")
+                    if (obj.classificationCategory != FirebaseVisionObject.CATEGORY_UNKNOWN) {
+                        val confidence: Int = obj.classificationConfidence!!.times(100).toInt()
+                        Log.d(LOG_MOD, "  Confidence: ${confidence}%")
+                    }
+                    Log.d(
+                        LOG_MOD,
+                        "  boundingBox: (${box.left}, ${box.top}) - (${box.right},${box.bottom})"
+                    )
+                }
+            }
+            .addOnFailureListener {
+                // Task failed with an exception
+                Toast.makeText(
+                    activity, "Oops, something went wrong!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+    }
+
+    private fun runOnCloudObjectDetection() {
+        val LOG_COD = "MLKit-OCD"
+        val image = FirebaseVisionImage.fromBitmap(cameraImage)
+        val detector = FirebaseVision.getInstance().cloudImageLabeler
+        detector.processImage(image)
+            .addOnSuccessListener { objects ->
+                for ((idx, obj) in objects.withIndex()) {
+                    Log.d(LOG_COD, "Detected object: ${idx} ")
+                    Log.d(LOG_COD, "Name: ${obj.text}")
+                    Log.d(LOG_COD, "Confidence: ${obj.confidence}")
+                    Log.d(LOG_COD, "EntityId: ${obj.entityId}")
+                }
+            }
+            .addOnFailureListener { e ->
+                // Task failed with an exception
+                Toast.makeText(
+                    activity, "Oops, something went wrong!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
     }
 }
