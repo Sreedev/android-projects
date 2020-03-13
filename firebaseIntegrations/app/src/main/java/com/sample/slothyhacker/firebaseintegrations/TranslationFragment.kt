6c37730b3d7a95f732bc63c75edea59bcd669b36
Sreedev <sreedev.r5@gmail.com>
2020-03-13 18:04:37 +0530

    Added a method

--- firebaseIntegrations/app/src/main/java/com/sample/slothyhacker/firebaseintegrations/TranslationFragment.kt
+++ firebaseIntegrations/app/src/main/java/com/sample/slothyhacker/firebaseintegrations/TranslationFragment.kt
@@ -1,10 +1,12 @@
 package com.sample.slothyhacker.firebaseintegrations
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_translation.*
 
 
 class TranslationFragment : Fragment() {
@@ -18,5 +20,14 @@ class TranslationFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
+
+        button_translate.setOnClickListener {
+            view?.let { translateToReqLang(et_text_to_translate.text)}
+        }
     }
+
+    private fun translateToReqLang(textToTranslate: Editable) {
+
+    }
+
 }
